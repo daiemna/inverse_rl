89551c19610bb035faa188512feae21872063c20
Daiem Ali <daiem.ali@recogizer.de>
2018-06-21 17:09:08 +0200

    working dynamics model

--- main.py
+++ main.py
@@ -3,36 +3,36 @@ from sklearn.model_selection import ParameterGrid
 import pandas as pd
 # import matplotlib.pylab as plt
 import time
+from pylogging import setup_logger, HandlerType
 
-from scripts.pid_pendulum import PendulumPID
+from scripts.pid_pendulum import PendulumPID, do_experiment
 # found params.
 # Kp    Ki    Kd
 # 0.23  0.51  0.28
 
 
 def main():
-    min_val, max_val, stride = 0.0, 1.0, 0.01
-    total_exp = ((max_val-min_val)/stride)**3
+    setup_logger(log_directory='./logs', file_handler_type=HandlerType.ROTATING_FILE_HANDLER, allow_console_logging=False, console_log_level="WARNING")
+    min_val, max_val, stride = -1.0, 1.0, 0.1
+    total_exp = int(((max_val-min_val)/stride)**3)
     param_grid = {
         "Kp": np.arange(min_val, max_val, stride),
         "Ki": np.arange(min_val, max_val, stride),
         "Kd": np.arange(min_val, max_val, stride)
     }
+
     target = 0
+    pid_controller = PendulumPID(1, 0, 0, target=target)
+
     score_list = []
     i = 0
     for params in list(ParameterGrid(param_grid)):
-        pid_cont = PendulumPID(params["Kp"], params["Ki"], params["Kd"], target)
-        done = False
+        avg_reward = do_experiment(pid_controller, params["Kp"], params["Ki"], params["Kd"])
         
-        creward = 0.0
-        while not done:
-            obs, reward, done, info = pid_cont.step()
-            creward += reward
         i += 1
-        pid_cont.env.terminate()
+        # pid_controller.env.terminate()
         print("---------------------------Experiment {0}/{1}".format(i, total_exp))
-        score_list.append((creward, params["Kp"], params["Ki"], params["Kd"]))
+        score_list.append((np.round(avg_reward, 2), np.round(params["Kp"], 2), np.round(params["Ki"], 2), np.round(params["Kd"], 2)))
     
     df = pd.DataFrame(data=score_list, columns=["score", "Kp", "Ki", "Kd"])
     max_score = max(df["score"])
@@ -41,29 +41,27 @@ def main():
     print("Winner is : ")
     print(sel_df)
     df.index = df["score"]
-    print(df.head(10))
+    df.sort_index(ascending=False, inplace=True)
+    print(df[["Kp", "Ki", "Kd"]].head(10))
     # df[["Kp", "Ki", "Kd"]].plot()
     # df.plot()
     # plt.show()
 
 
 def main2():
-    # last search:
-    # https://www.myphysicslab.com/pendulum/pendulum-en.html
-    # https://github.com/openai/gym/blob/master/gym/envs/classic_control/pendulum.py
-    # google: pendulum mass length speed relation
-    # pid_cont = PendulumPID(0.23,  0.51,  0.28, 0.0)
-    pid_cont = PendulumPID(0.1, 0.0, 0.0, 0.0)
+
+    setup_logger(log_directory='./logs', file_handler_type=HandlerType.ROTATING_FILE_HANDLER, allow_console_logging=False, console_log_level="DEBUG")
+    pid_cont = PendulumPID(2.0, 0, 0, config_path="pid_constants.yml")
     done = False
     i = 0
     creward = 0.0
     while not done:
-        pid_cont.env.render()
+        pid_cont.env.render(mode='human')
         obs, reward, done, info = pid_cont.step()
         # print("theta : {0} , torueq : {1}".format(np.arccos(obs[0]), info["action"]))
         creward += reward
         i += 1
-        # time.sleep(1)
+        time.sleep(0.5)
     print("cumulative reward: {0}".format(creward))
     pid_cont.env.terminate()
 
--- scripts/pid_pendulum.py
+++ scripts/pid_pendulum.py
@@ -1,54 +1,98 @@
 # from sandbox.rocky.tf.algos.trpo import TRPO
 from sandbox.rocky.tf.envs.base import TfEnv
-from rllab.envs.gym_env import GymEnv
+# from rllab.envs.gym_env import GymEnv
+from gym.envs.classic_control.pendulum import PendulumEnv
 import numpy as np
+import logging
+from ruamel.yaml import YAML
+
+logger = logging.getLogger(__name__)
 
 
 class PendulumPID(object):
 
-    def __init__(self, Kp, Ki, Kd, target, **kwargs):
-        self._env = TfEnv(GymEnv('Pendulum-v0',
-                                 record_video=kwargs.get("record_video", False),
-                                 record_log=kwargs.get("record_log", False)))
+    def __init__(self, Kp, Ki, Kd, **kwargs):
+        # self._env = TfEnv(GymEnv('Pendulum-v0',
+        #                          record_video=kwargs.get("record_video", False),
+        #                          record_log=kwargs.get("record_log", False)))
+        self._env = PendulumEnv()
+        config_path = kwargs.get("config_path", None)
+        config = {}
+        if config_path is not None:
+            config = read_yaml_file(config_path)
+        
+        self._alpha_tol = kwargs.get("stability_region", config.get("stablity-active-region", 0.5))
+        self._dt = kwargs.get("time_delta", config.get("time-delta", 1.0))
+        self._length = kwargs.get("length", config.get("length", 1.0))
+        self._mass = kwargs.get("mass", config.get("mass", 1.0))
+        self._g = kwargs.get("g_val", config.get("gravitation", 10.0))
+        self._target = kwargs.get("target", config.get("target-angle", 0.0))
+
+        self.reset(Kp, Ki, Kd)
+        
+    def reset(self, Kp, Ki, Kd):
         self._int, self._diff = 0.0, 0.0
-        self._Kp, self._Ki, self._Kd, self._target = Kp, Ki, Kd, target
-        self._last_obs = self._env.reset()
+        self._Kp, self._Ki, self._Kd = Kp, Ki, Kd
+        self._last_obs = self._env.reset(high=np.array([3, 1]))
         self._alpha_dot_prev = self._last_obs[2]
-        self._alpha_tol = 0.2
-
-
+    
     @property
     def env(self):
         return self._env
 
     def step(self):
-        r = 1.0
-        Ip = r/2.0
-        m = 1.0
-        g = 10.0
-        dt = 2.0
-
+        Ip = self._length / 2.0
         alpha, alpha_dot = np.arccos(self._last_obs[0]), self._last_obs[2]
-        alpha_dotdot = (alpha_dot - self._alpha_dot_prev)/dt
+        alpha_dotdot = (alpha_dot - self._alpha_dot_prev)/self._dt
         self._alpha_dot_prev = alpha_dot
 
-        taw_stablity = (m * alpha_dotdot * Ip**2) - (m * Ip * np.sin(alpha) * alpha_dot * r) - (m * g * Ip * np.sin(alpha))
+        # taw_stability = (self._mass * alpha_dotdot * Ip**2) - (self._mass * np.sin(alpha) * alpha_dot * self._length**2 * 0.5) - \
+        #                 (self._mass * self._g * Ip * np.sin(alpha))
 
-        if abs(alpha - self._target) > self._alpha_tol:
-            new_taw = -np.sign(alpha_dot) * (taw_stablity)
-        else:
-            error = taw_stablity
+        if abs(alpha - self._target) > self._alpha_tol:  # swing up
+            taw_stability = (self._mass * alpha_dotdot * Ip**2) - (self._mass * alpha_dot**2 * self._length**2 * 0.5) - \
+                            (self._mass * self._g * Ip * np.sin(alpha))
+            new_taw = -np.sign(alpha_dot) * (taw_stability)
+        else:                                            # stabilization pid
+            taw_stability = (self._mass * alpha_dotdot * Ip**2) - (self._mass * alpha_dot**2 * self._length**2 * 0.5) - \
+                            (self._mass * self._g * Ip * np.sin(alpha))
+            
+            error = np.sign(alpha_dot) * taw_stability
             self._int += error
             new_taw = self._Kp * error + self._Ki * self._int + self._Kd * (error - self._diff)
             self._diff = error
-
+            logger.debug("alpha : %f" % alpha)
+            # logger.debug("alpha_dot : %f" % alpha_dot)
+            # logger.debug("alpha_dotdot : %f" % alpha_dotdot)
+            logger.debug("KE : %f" % (self._mass * alpha_dot**2 * self._length**2 * 0.5))
+            logger.debug("Inertia : %f" % (self._mass * alpha_dotdot * Ip**2))
+            logger.debug("PE : %f" % (self._mass * self._g * Ip * np.sin(alpha)))
+            logger.debug("taw : %f" % new_taw)
+            logger.debug("------------------------------------------")
         self._last_obs, r, d, info = self._env.step([new_taw])
 
-        print("alpha : %f" % alpha)
-        print("alpha_dot : %f" % alpha_dot)
-        print("alpha_dotdot : %f" % alpha_dotdot)
-        print("taw : %f" % new_taw)
-        print("------------------------------------------")
         info.update({"action": new_taw})
 
         return self._last_obs, r, d, info
+
+
+def do_experiment(pid_controller, Kp, Ki, Kd, exp_count=10):
+    avg_creward = 0.0
+    for i in np.arange(exp_count):
+        pid_controller.reset(Kp, Ki, Kd)
+        done = False
+        
+        creward = 0.0
+        while not done:
+            obs, reward, done, info = pid_controller.step()
+            creward += reward
+
+        avg_creward += creward
+    return (avg_creward/float(exp_count))
+
+
+def read_yaml_file(path):
+    """Reads a yaml file."""
+    with open(path, 'r') as stream:
+        yml = YAML(typ='safe')
+        return yml.load(stream)
